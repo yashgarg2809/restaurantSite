26feee9d919d3dddd2d858141adde0145d919ead
Yash Garg <yashgarg2809@gmail.com>
2022-08-06 15:19:27 +0530

    Hi

--- js/orderOnline.js
+++ js/orderOnline.js
@@ -1,41 +1,45 @@
 const foodArr = Array(100);
-let count=0;
+let count = 0;
 let cartOpened = false;
 var ul;
 // var r = document.querySelector(":root");
 // var rs = getComputedStyle(r);
+let goToCartText;
 
-$(document).ready(function(){
-    $("#cartPopup").hide();
-    ul =  document.getElementById("cartUL");
+$(document).ready(function () {
+  $("#cartPopup").hide();
+  ul = document.getElementById("cartUL");
 });
 
-$(".addButton").click(function(){
-    foodArr[count]=$(this).parent().children("span").children("span").text();
-    count++;
-    console.log("Food arr is "+foodArr);
+$(".addButton").click(function () {
+  foodArr[count] = $(this).parent().children("span").children("span").text();
+  count++;
+  console.log("Food arr is " + foodArr);
+  $("#cartPopup").hide();
+  $("#goToCartButton").text("Go to Cart(" + count + ")");
 });
 
-
-$("#goToCartButton").click(function(){
-    if(cartOpened==false){
+$("#goToCartButton").click(function () {
+  if (cartOpened == false) {
     $("#cartPopup").show();
+
     $("#goToCartButton").text("Close Cart");
-    foodArr.forEach(item => {
-        var li = document.createElement("li");
-        li.innerHTML = item;
-        ul.append(li);
-        
+    foodArr.forEach((item) => {
+      var li = document.createElement("li");
+      li.innerHTML = item;
+      ul.append(li);
     });
-    cartOpened=true;
-}
-else{
-    cartOpened=false;
+    cartOpened = true;
+  } else {
+    cartOpened = false;
     $("#cartPopup").hide();
     ul.innerHTML = "";
-    $("#goToCartButton").text("Go to Cart");
-    }
-    
+    console.log(goToCartText);
+    $("#goToCartButton").text("Go to Cart.");
+  }
 });
 
-
+$("#orderCompleteButtonFinal").click(function () {
+  $("#cartPopup").hide();
+  alert("Order placed successfully");
+});
